3715d085fbd968ed30eda86d67e7ac1ea0f2bc51
Tihomir Trifonov <t.trifonov@gmail.com>
2014-06-11 19:19:53 +0300

    fixed keypair handling

--- dashboard/aws_provision_libcloud.py
+++ dashboard/aws_provision_libcloud.py
@@ -8,14 +8,15 @@ from libcloud.compute.providers import get_driver
 
 def provision(access_id,
               secret_api_key,
-              keypair_name=None,
               node_name="Node",
+              keypair_name=None,
               sec_groups=['default'],
               image_id='ami-b6bdde86',
-              size='t1.micro'):
+              size='t1.micro',
+              timeout=60, **kw):
     # A list of security groups you want this node to be added to
     #sec_groups = ['secgroup1', 'secgroup2']
-
+    print "Starting provisioning..."
     cls = get_driver(Provider.EC2_US_WEST_OREGON)
     driver = cls(access_id, secret_api_key)
 
@@ -24,8 +25,16 @@ def provision(access_id,
     size = [s for s in sizes if s.id == size][0]
 
     image = [s for s in images if s.id == image_id][0]
-
+    kwargs = {}
+    kwargs.update(kw)
+    if keypair_name:
+        kwargs['ex_keyname'] = keypair_name
     node = driver.create_node(name=node_name, image=image, size=size,
-                              ex_keyname=keypair_name,
-                              ex_securitygroup=sec_groups)
+                              ex_securitygroup=sec_groups, **kwargs)
+    try:
+        driver.wait_until_running([node], wait_period=5, timeout=timeout)
+        print "Provisioning finished"
+    except Exception, ex:
+        print "Provisioning failed, %r" % ex
+
     return node

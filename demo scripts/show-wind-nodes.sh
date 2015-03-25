aae17ead53b5e1eb5d0f7aac3867305e60f6f8f3
Tihomir Trifonov <t.trifonov@gmail.com>
2015-03-25 17:25:59 +0200

    Updated win script

--- demo scripts/show-wind-nodes.sh
+++ demo scripts/show-wind-nodes.sh
@@ -1,6 +1,7 @@
 #! switch [os=windows]
 #! /usr/bin/ps1
 
-& "cloudrunner-node details"
-
 [Environment]::OSVersion
+
+$process = Start-Process -FilePath C:\Python27\Scripts\cloudrunner-node.exe -ArgumentList details -NoNewWindow -PassThru -Wait
+$process.ExitCode
